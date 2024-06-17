b264ca13c11e324592f6953ff8ce205b28873926
Chirag Juneja <chiragj1901@gmail.com>
2024-06-17 12:06:32 +0530

    redone

---
+++ script.js
@@ -0,0 +1,13 @@
+
+(function () {
+    var e = document.createElement('iframe');
+    e.src = 'https://discord.com/widget?id=1113056062128533586&theme=dark'; 
+    e.width = '350';
+    e.height = '500';
+    e.frameBorder = '0';
+    e.setAttribute('allowtransparency', 'true');
+    e.style.display = 'block';
+    e.style.margin = '0 auto';
+    document.getElementById('discord-widget').appendChild(e);
+  })();
+  
